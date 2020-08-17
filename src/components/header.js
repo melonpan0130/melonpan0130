0329d8d9bfc4704519004ca386669ea48ed0c1d8
Eunju/kiori <eun01300714@gmail.com>
2020-08-17 22:08:24 +0900

    menubar

--- src/components/header.js
+++ src/components/header.js
@@ -3,9 +3,9 @@ import PropTypes from "prop-types"
 import React from "react"
 
 import "./header.css"
-import "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
 
 const Header = ({ siteTitle, siteDescription }) => (
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous" />
   <header id="header">
     <div id="title">
       <h1>

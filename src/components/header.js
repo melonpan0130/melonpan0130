1bb5f9c6529621edced03ed838d7df775d5fa685
Eunju/kiori <eun01300714@gmail.com>
2020-08-17 21:59:11 +0900

    menubar

--- src/components/header.js
+++ src/components/header.js
@@ -3,14 +3,20 @@ import PropTypes from "prop-types"
 import React from "react"
 
 import "./header.css"
+import "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
 
 const Header = ({ siteTitle, siteDescription }) => (
   <header id="header">
-    <div>
+    <div id="title">
       <h1>
         <Link id="titleLink" to="/">{siteTitle}</Link>
       </h1>
       <p>{siteDescription}</p>
+    </div>
+    <div class="btn-group" role="group" aria-label="Basic example">
+      <button type="button" class="btn btn-secondary">Economy</button>
+      <button type="button" class="btn btn-secondary">News</button>
+      <button type="button" class="btn btn-secondary">Etc</button>
     </div>
   </header>
 )
